89dec1babf0ddfd77a5c6361340f056629d1fee4
Djo Mwanza <djobukata@gmail.com>
2018-09-12 20:38:19 +0200

    Added git ignore

---
+++ tests/demo.js
@@ -0,0 +1,15 @@
+module.exports = {
+    '@tags': ['demo'],
+    'Demo test Google' : function (client) {
+        client
+            .url('http://www.google.com')
+            .waitForElementVisible('body', 1000)
+            .assert.title('Google')
+            .assert.visible('input[type=text]')
+            .setValue('input[type=text]', 'Djo Mwanza')
+            .waitForElementVisible('input[name=btnK]', 1000)
+            .click('input[name=btnK]')
+            .pause()
+            .end();
+    }
+};
